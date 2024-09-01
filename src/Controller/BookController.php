cb98a7cc70743144de5733073d6594f1e1994a8d
sanemugur <sanem.ugur@marsneo.com>
2024-09-02 02:36:27 +0300

    src/Controller/BookController.php file created using php bin/console make:controller

---
+++ src/Controller/BookController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookController extends AbstractController
+{
+    #[Route('/books', name: 'app_books')]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+}
