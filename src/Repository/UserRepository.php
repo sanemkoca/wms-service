e0de6213d2f06a50d2329b28ca7b6c4d6977e79d
sanemugur <sanem.ugur@marsneo.com>
2024-09-02 02:43:49 +0300

    src/Repository/UserRepository.php file modified and findUserById function added.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -16,28 +16,11 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findUserById($userId): User
+    {
+        /** @var User $user */
+        $user = $this->getEntityManager()->getRepository(User::class)->find($userId);
 
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        return $user;
+    }
 }
