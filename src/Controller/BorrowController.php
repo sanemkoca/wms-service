ad2e7cb8cd379eed5f2a37405fe13ac5c52fde91
sanemugur <sanem.ugur@marsneo.com>
2024-09-02 03:04:11 +0300

    src/Controller/BorrowController.php file modified and add function created.

--- src/Controller/BorrowController.php
+++ src/Controller/BorrowController.php
@@ -8,11 +8,28 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class BorrowController extends AbstractController
 {
-    #[Route('/borrows', name: 'app_borrows')]
-    public function index(): Response
+    #[Route('/borrow/add', name: 'borrow_book', methods: ['GET', 'POST'])]
+    public function add(Request $request, ManagerRegistry $registry): Response
     {
-        return $this->render('borrow/index.html.twig', [
-            'controller_name' => 'BorrowController',
+        $bookRepository = new BookRepository($registry);
+        $book = $bookRepository->findBookById($request->query->get('recordId'));
+
+        $borrowRecord = new BorrowRecord();
+        if ($book) {
+            $borrowRecord->setBookId($book);
+        }
+
+        $form = $this->createForm(BorrowRecordType::class, $borrowRecord);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $borrowRecordRepository = new BorrowRecordRepository($registry);
+            $borrowRecordRepository->borrowBook($borrowRecord);
+            return $this->redirectToRoute('book_list');
+        }
+
+        return $this->render('borrow/add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
