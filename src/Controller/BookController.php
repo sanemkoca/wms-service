5042eda2bcecc0d59adb9133070748e69efd0ea5
sanemugur <sanem.ugur@marsneo.com>
2024-09-02 03:03:31 +0300

    src/Controller/BookController.php file modified and add function created.

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,5 +19,23 @@ class BookController extends AbstractController
         $bookRepository = new BookRepository($registry);
         $list = $bookRepository->listBook();
         return $this->render('book/list.html.twig', ['books' => $list]);
+    }
+
+    #[Route('/book/add', name: 'book_add', methods: ['GET', 'POST'])]
+    public function add(Request $request, ManagerRegistry $registry): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bookRepository = new BookRepository($registry);
+            $bookRepository->createBook($book);
+            return $this->redirectToRoute('book_list');
+        }
+
+        return $this->render('book/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
